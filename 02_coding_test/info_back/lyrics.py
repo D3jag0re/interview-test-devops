955d5d3d693b24c2b4e5fb7c91ca17366229ef3f
D3jag0re <mbrinkwo@gmail.com>
2023-06-09 14:54:53 -0400

    push for Reabs

--- 02_coding_test/info_back/lyrics.py
+++ 02_coding_test/info_back/lyrics.py
@@ -9,11 +9,17 @@ import re
 token = constants.api_token 
 
 #-------------------------------------------------------------------------------------------------------------------------#
+# Define any global variables
+#-------------------------------------------------------------------------------------------------------------------------#
+
+lyrics_only = "" 
+
+#-------------------------------------------------------------------------------------------------------------------------#
 # Function to list songs by an artist by popularity
 #-------------------------------------------------------------------------------------------------------------------------#
 
 def top5_songs():
-    inp_artist = input("Type In The Name Of the Artist:")
+    inp_artist = input("\nType In The Name Of the Artist: ")
     genius= Genius(token)
     artist = genius.search_artist(inp_artist, max_songs=3, sort="popularity")
     print(artist.songs)
@@ -28,50 +34,37 @@ words_to_remove = ["i", "am", "we", "me", "my", "as", "the", "you", "and", "the"
 
 
 #-------------------------------------------------------------------------------------------------------------------------#
-# Main - Give the user two choices and execute accordingly
+# This function will be used to retrieve the lyrics for the selected song
 #-------------------------------------------------------------------------------------------------------------------------#
 
-opening_line = """---------------------------------------------------- \n
-Successful Launch! Please See Below For Instructions \n  
-----------------------------------------------------"""
-def main():
-    print(opening_line)
-    print("\nWhat would you like to do?\n")
-    print("1. Top 3 Most Popular Songs By An Artist")
-    print("2. Top 5 Most Used Words In a Song?\n")
-    choice = input("Please Make Your Selection:")
-    if choice == '1':
-        top5_songs()
-    if choice == '2':
-        print("ok DONE")
-    else:
-        print("Please Make a Selection Between 1 and 2")
-        return 
+def get_lyrics():
+    genius = Genius(token)
+    # User Inputs
+    artist= input("\nType In The Name Of the Artist: ") 
+    song_song= input("Type In The Name Of the Song: ")
 
+    # Searches for song and retrieves the lyrics
+    song = genius.search_song(song_song, artist)
+    s_lyrics = song.lyrics
 
-if __name__ == "__main__":
-    main()
+    # Take the output, take anything out of inside '[]' and then count words, print top 5 words. 
+    lyrics_only = re.sub("[\(\[].*?[\)\]]", "", s_lyrics)
+    lyrics_only = str(lyrics_only)
+    return lyrics_only
+    
 
-# Print Lyrics of a Song
-#genius = Genius(token)
-#artist = genius.search_artist("Metallica", max_songs=2, sort='popularity')
-#song = input('What is the song title you want lyrics for? ')
-#song = artist.song(song)
-#s_lyrics = song.lyrics
-#print(s_lyrics)
-
-# Take the output, take anything out of inside '[]' and then count words, print top 5 words. 
-
-#lyrics_only = re.sub("[\(\[].*?[\)\]]", "", s_lyrics)
-#lyrics_only = str(lyrics_only)
-#print(lyrics_only)
+#-------------------------------------------------------------------------------------------------------------------------#
 
 # This function:
-# Converts the lyrics into a one line string for easier parsing
-# Counts the remaining words
-# Calls on _function_ to remove entries
-# Outputs top 5 words per song with their word count
+#   Converts the lyrics into a one line string for easier parsing
+#   Counts the remaining words
+#   Calls on remove_words function to remove entries
+#   Outputs top 5 words per song with their word count
+
+#-------------------------------------------------------------------------------------------------------------------------#
+
 def word_count(str):
+
     str = str.lower()
     str = " ".join(str.splitlines()) 
     counts = dict()
@@ -89,13 +82,29 @@ def word_count(str):
 
     for tuple in top_5:
         print(*tuple)
-    
+        
+#-------------------------------------------------------------------------------------------------------------------------#
+# Main - Give the user two choices and execute above functions accordingly
+#-------------------------------------------------------------------------------------------------------------------------#
 
-
-
-
-# Outputs
-
-#print(s_lyrics)
-#print(lyrics_only)
-#print(word_count(lyrics_only))
+opening_line = """---------------------------------------------------- \n
+Successful Launch! Please See Below For Instructions \n  
+----------------------------------------------------"""
+def main():
+    print(opening_line)
+    print("\nWhat would you like to do?\n")
+    print("1. Top 3 Most Popular Songs By An Artist")
+    print("2. Top 5 Most Used Words In a Song" )
+    print("3. Get Lyrics To a Song\n")
+    choice = input("Please Make Your Selection: ")
+    if choice == '1':
+        top5_songs()
+    if choice == '2':
+        print(word_count(get_lyrics()))
+    if choice == '3':
+        print(get_lyrics())
+    #else:
+    #    print("Please Make a Selection Between 1 and 2")
+         
+if __name__ == "__main__":
+    main()
