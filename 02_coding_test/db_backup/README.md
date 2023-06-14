2701b8f6d68aec5956810fb7b550ae8be97901c7
D3jag0re <mbrinkwo@gmail.com>
2023-06-14 14:34:29 -0400

    started db backup

--- 02_coding_test/db_backup/README.md
+++ 02_coding_test/db_backup/README.md
@@ -1,2 +1,13 @@
 # DB Backup 
 
+As per the requirements this will back up a database and send the output to a cloud storage solution. 
+
+For this I will be backing up a "dummy" database locally, and sending the output to an Azure Blob Storage Account. 
+
+# Get Started 
+
+## Get a Database 
+
+## Create your vars file 
+
+## Run The Code  
---
+++ 02_coding_test/db_backup/backup.py
@@ -0,0 +1,24 @@
+import pyodbc 
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import var 
+# Database Connection Details 
+print(var.db_server)
+
+
+# Connect To The Database 
+
+
+# Perform Backup And Save Locally 
+
+
+# Connect To Azure Blob Storage
+
+
+# Upload The Backup File To Azure Blob Storage 
+
+
+# Delete The Local Backup 
+
+
+# Close DB Connection
+#conn.close()
