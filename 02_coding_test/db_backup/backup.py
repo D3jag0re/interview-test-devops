b412cde398ad508e24969d3114651e022d09645c
D3jag0re <mbrinkwo@gmail.com>
2023-06-15 13:43:36 -0400

    local backup working

--- 02_coding_test/db_backup/backup.py
+++ 02_coding_test/db_backup/backup.py
@@ -1,4 +1,6 @@
 import pyodbc 
+import subprocess
+import mysql
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 import var 
 # Database Connection Details 
@@ -6,11 +8,16 @@ print(var.db_server)
 
 
 # Connect To The Database (pyodbc.connect(string?))
+
 connection_string = f"Driver={{MySQL ODBC 8.0 Unicode Driver}};Server={var.db_server};Database={var.db_name};Uid={var.db_username};Pwd={var.db_password}"
 conn = pyodbc.connect(connection_string)
-cursor = conn.cursos() 
+cursor = conn.cursor() 
 
-# Perform Backup And Save Locally 
+# Perform Backup And Save Locally (if no shell = True will need --result-file=<path>)
+
+#backup_file_path = 'C:/temp'
+backup_command = f'mysqldump -u{var.db_username} -p{var.db_password} world > c:/temp/test.sql'
+subprocess.run(backup_command, shell=True, check =True)
 
 
 # Connect To Azure Blob Storage
